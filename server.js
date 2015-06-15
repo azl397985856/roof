59d6ab891378a6fd6dec8979103531055c87bc24
加缪 <jiamiu.hzy@alibaba-inc.com>
2015-06-15 15:48:40 +0800

    dir refactory complete

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ new WebpackDevServer(webpack(config), {
 
     console.log('Listening at localhost:9000');
   });
+
+
+var Bus = require("roof/lib/mixin/react.js")
+var Bus = require("roof/lib/util/serverRendering.js")
+
+var Bus = require("roof").Bus
+var Bus = require("roof").Node
+var Bus = require("roof").Nodes
