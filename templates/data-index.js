c0c5417839967bfc50a0a364a2f07e7205844861
加缪 <jiamiu.hzy@alibaba-inc.com>
2015-06-29 16:05:20 +0800

    more elegent data center template

--- templates/data-index.js
+++ templates/data-index.js
@@ -1,54 +1,16 @@
 'use strict';
-let assign = require('object-assign');
-let RoofMixin = require('roof/lib/mixin/react.js');
+
+function zipObject(keys, values) {
+  let output = {};
+  for (let i in keys) {
+    output[keys[i]] = values[i];
+  }
+  return output;
+}
+
 let dataNames = require('../roof.json').modules.data;
+let dataModules = zipObject(dataNames, dataNames.map(function(dataName) {
+  return require('./' + dataName);
+}))
 
-function zipObject(keys, values){
-  let output = {}
-  for(let i in keys){
-    output[keys[i]] = values[i]
-  }
-  return output
-}
-
-let exportsObj = mixinFactory;
-
-function mixinFactory(mixinDef) {
-
-  if (!exportsObj.isServerRendering) {
-    if (Object.keys(exportsObj.source).length === 0) {
-      assign(exportsObj.source, zipObject(dataNames, dataNames.map(function(name) {
-        return require('./' + name)({context: context});
-      })));
-    }
-  }else {
-    // 占位符
-    exportsObj.source = {};
-  }
-
-  return new RoofMixin(exportsObj.isServerRendering ? exportsObj : exportsObj.source, mixinDef);
-}
-
-// for server rendering
-exportsObj._serverRenderingData = {};
-exportsObj.isServerRendering = false;
-
-exportsObj.setData = function(key, context) {
-  exportsObj._serverRenderingData[key] = zipObject(dataNames, dataNames.map(function(name) {
-    return require('./' + name)({context: context});
-  }));
-};
-
-exportsObj.getData = function(key) {
-  return exportsObj._serverRenderingData[key];
-};
-
-exportsObj.removeData = function(key) {
-  delete exportsObj._serverRenderingData[key];
-};
-
-//  占位符
-exportsObj.source = {};
-
-
-module.exports = exportsObj;
+module.exports = require('roof/lib/data')(dataModules)
